041effe7690528c12c53daef022a95ef33ee7cb4
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-07 17:54:46 -0800

    initial commit

---
+++ src/main/java/com/vcabading/pokebook/PokebookApplication.java
@@ -0,0 +1,17 @@
+package com.vcabading.pokebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/////////////////////////////////////////////////////////////////
+//	POKEBOOK APPLICATION
+/////////////////////////////////////////////////////////////////
+
+@SpringBootApplication
+public class PokebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokebookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vcabading/pokebook/PokebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vcabading.pokebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
