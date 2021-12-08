0b502c0fe4b052d3d4566bac2c54564f756d97c4
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-07 22:10:51 -0800

    create ExpenseRepository

--- src/main/java/com/vcabading/pokebook/models/Expense.java
+++ src/main/java/com/vcabading/pokebook/models/Expense.java
@@ -3,9 +3,13 @@ package com.vcabading.pokebook.models;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -16,6 +20,8 @@ import org.springframework.format.annotation.DateTimeFormat;
 // 	EXPENSE CLASS
 //////////////////////////////////////////////////////////////
 
+@Entity
+@Table(name="expense")
 public class Expense {
 
 	//	//// FIELDS //////////////////////////////////////////
@@ -59,6 +65,74 @@ public class Expense {
 		this.vendor = vendor;
 		this.amount = amount;
 		this.description = description;
+	}
+	
+	@PrePersist
+	protected void onCreate()	{
+		this.createdAt = new Date();
+	}
+	
+	@PreUpdate
+	protected void onUpdate() {
+		this.updatedAt = new Date();
+	}
+	
+	//	//// GETTERS AND SETTERS //////////////////////////////
+
+	public long getID() {
+		return ID;
+	}
+
+	public void setID(long iD) {
+		ID = iD;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getVendor() {
+		return vendor;
+	}
+
+	public void setVendor(String vendor) {
+		this.vendor = vendor;
+	}
+
+	public float getAmount() {
+		return amount;
+	}
+
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
 	}
 	
 	
---
+++ src/main/java/com/vcabading/pokebook/repositories/ExpenseRepository.java
@@ -0,0 +1,21 @@
+package com.vcabading.pokebook.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.vcabading.pokebook.models.Expense;
+
+////////////////////////////////////////////////////////////////
+//	EXPENSE REPOSITORY
+////////////////////////////////////////////////////////////////
+
+@Repository
+public interface ExpenseRepository extends CrudRepository<Expense, Long> {
+
+	//	---- Retrieves all books -------------------------------
+	List<Expense> findAll();
+	
+	List<Expense> 
+}
